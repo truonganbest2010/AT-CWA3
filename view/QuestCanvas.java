b4c338300dfc355c1990b96de1db63ef8a01f3b6
AnTruong <truonganbest2010@gmail.com>
2020-09-21 15:57:11 -0500

    m

--- view/QuestCanvas.java
+++ view/QuestCanvas.java
@@ -24,26 +24,30 @@ public class QuestCanvas extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
 
         if (panel.getGameState() == GameState.READY){
-
+            g2.setColor(Color.white);
+            g2.drawOval(23, 20, 60, 60);
+            g2.setFont(new Font("Courier", Font.BOLD, 15));
+            g2.drawString("^", 40, 45);
+            g2.drawString("^", 60, 45);
+            g2.drawString("___", 40, 60);
         }
-
         else if (panel.getGameState() == GameState.PLAYING){
             if (colorQuest != null){
                 g2.setColor(colorQuest);
             }
             if (shapeNo == 0){
-                g2.drawOval(30, 30, 60, 60);
-                g2.fillOval(30+2, 30+2, 60-4, 60-4);
+                g2.drawOval(25, 30, 60, 60);
+                g2.fillOval(25+2, 30+2, 60-4, 60-4);
             } else if (shapeNo == 1){
-                g2.drawRect(30, 30, 60, 60);
-                g2.fillRect(30+2, 30+2, 60-4, 60-4);
+                g2.drawRect(25, 30, 60, 60);
+                g2.fillRect(25+2, 30+2, 60-4, 60-4);
             } else if (shapeNo == 2){
-                int[] x = new int[3]; x[0]=30; x[1]=30; x[2]=30+60;
-                int[] y = new int[3]; y[0]=30; y[1]=30+60; y[2]=30+60;
+                int[] x = new int[3]; x[0]=25; x[1]=25; x[2]=25+60;
+                int[] y = new int[3]; y[0]=25; y[1]=25+60; y[2]=25+60;
                 g2.fillPolygon(x, y, 3);
             } else if (shapeNo == 3){
-                int[] x = new int[3]; x[0]=30+60; x[1]=30; x[2]=30+60;
-                int[] y = new int[3]; y[0]=30; y[1]=30+60; y[2]=30+60;
+                int[] x = new int[3]; x[0]=25+60; x[1]=25; x[2]=25+60;
+                int[] y = new int[3]; y[0]=25; y[1]=25+60; y[2]=25+60;
                 g2.fillPolygon(x, y, 3);
             }
         }
